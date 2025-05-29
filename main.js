bbd0ef1c4136f1735550960f52040272c488a521
hackerbro0707 <dipanshuchaudhary192@gmail.com>
2025-05-29 17:23:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+
+document.getElementById('root').innerHTML = '<h1 style="text-align:center;">Aviator Tracker Pro (Static)</h1><p style="text-align:center;">React-based app needs to be deployed via React build tools like Vercel with framework detection or GitHub integration.</p>';
